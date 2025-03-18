91ba016a8f888447c7ae1a9aaa2b8d28e6b58a44
mu5dvlp <mu5dvlp@gmail.com>
2025-03-18 17:53:36 +0900

    Init repository

---
+++ content.js
@@ -0,0 +1,37 @@
+class Faker {
+    #xpath;
+    #text;
+
+    constructor(xpath, text) {
+        this.#xpath = xpath;
+        this.#text = text;
+    }
+
+    generateText() {
+        return this.#text;
+    }
+
+    fakeText() {
+        let element = document.evaluate(this.#xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if (element) {
+            element.textContent = this.#text;
+        }
+    }
+}
+
+//＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// Parameters
+let fakers = [
+    new Faker('//*[@id="asset_total_amount"]', "999,999,999")
+];
+
+//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+window.addEventListener("load", () => {
+    let fakers = [
+        new Faker('//*[@id="asset_total_amount"]', "999,999,999")
+    ];
+
+    fakers.forEach(faker => {
+        faker.fakeText();
+    });
+});
